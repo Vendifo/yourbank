e96eb46a4ca7e7bd587746ede8cd7d30632734c6
Vendigo <119477860+Vendifo@users.noreply.github.com>
2023-12-10 06:58:50 +0900

    Сделан функционал для компонента Questions а так же исправлены баги у компонента Features

--- yourbank/src/components/home/features/Features.jsx
+++ yourbank/src/components/home/features/Features.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './features.css';
 import featuresArrow from '../../../img/Assets/featuresArrow.svg';
 
 function Features() {
   const [selectedFeature, setSelectedFeature] = useState('Online banking');
+
+  useEffect(() => {
+    setSelectedFeature('Online Banking');
+  }, []);
+
 
   const handleButtonClick = (feature) => {
     setSelectedFeature(feature);
@@ -32,156 +37,157 @@ function Features() {
             <button onClick={() => handleButtonClick('Customer Support')} className={selectedFeature === 'Customer Support' ? 'active' : ''}>
               Customer Support
             </button>
+
           </div>
           <div className='features__content'>
-            {/* Conditional rendering based on selectedFeature */}
+            
             {selectedFeature === 'Online Banking' && (
               <div className='features__content'>
-              <div className='features__content-cards'>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>24/7 Account Access</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
+                <div className='features__content-cards'>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>24/7 Account Access</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Enjoy the convenience of accessing your accounts anytime, anywhere through our secure online banking
+                      platform. Check balances, transfer funds, and pay bills with ease.
+                    </span>
                   </div>
-                  <span>
-                    Enjoy the convenience of accessing your accounts anytime, anywhere through our secure online banking
-                    platform. Check balances, transfer funds, and pay bills with ease.
-                  </span>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>Mobile Banking App</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Stay connected to your finances on the go with our user-friendly mobile banking app. Easily manage
+                      your accounts, deposit checks, and make payments from your smartphone or tablet.
+                    </span>
+                  </div>
                 </div>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>Mobile Banking App</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
+                <div className='features__content-cards'>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>Secure Transactions</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Rest assured knowing that your transactions are protected by industry-leading security measures. We employ
+                      encryption and multi-factor authentication to safeguard your financial information.
+                    </span>
                   </div>
-                  <span>
-                    Stay connected to your finances on the go with our user-friendly mobile banking app. Easily manage
-                    your accounts, deposit checks, and make payments from your smartphone or tablet.
-                  </span>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>Bill Pay and Transfers</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Save time and avoid late fees with our convenient bill pay service. Set up recurring payments or make
+                      one-time transfers between your accounts with just a few clicks.
+                    </span>
+                  </div>
                 </div>
               </div>
-              <div className='features__content-cards'>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>Secure Transactions</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
-                  </div>
-                  <span>
-                    Rest assured knowing that your transactions are protected by industry-leading security measures. We employ
-                    encryption and multi-factor authentication to safeguard your financial information.
-                  </span>
-                </div>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>Bill Pay and Transfers</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
-                  </div>
-                  <span>
-                    Save time and avoid late fees with our convenient bill pay service. Set up recurring payments or make
-                    one-time transfers between your accounts with just a few clicks.
-                  </span>
-                </div>
-              </div>
-            </div>
-            
-              
+
+
             )}
             {selectedFeature === 'Financial Tools' && (
               <div className='features__content'>
-              <div className='features__content-cards'>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>Financial Planning</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
+                <div className='features__content-cards'>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>Financial Planning</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Achieve your financial goals with our comprehensive financial planning tools. Plan for your future,
+                      track your expenses, and make informed decisions about your investments.
+                    </span>
                   </div>
-                  <span>
-                    Achieve your financial goals with our comprehensive financial planning tools. Plan for your future,
-                    track your expenses, and make informed decisions about your investments.
-                  </span>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>Budgeting Made Easy</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Take control of your finances with our easy-to-use budgeting tools. Set budgets, track your spending,
+                      and receive personalized insights to help you manage your money effectively.
+                    </span>
+                  </div>
                 </div>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>Budgeting Made Easy</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
+                <div className='features__content-cards'>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>Investment Insights</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Stay informed about market trends and make smart investment decisions with our investment insights.
+                      Access real-time data, research potential opportunities, and optimize your investment portfolio.
+                    </span>
                   </div>
-                  <span>
-                    Take control of your finances with our easy-to-use budgeting tools. Set budgets, track your spending,
-                    and receive personalized insights to help you manage your money effectively.
-                  </span>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>Expense Tracking</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Simplify your financial life by tracking your expenses effortlessly. Categorize your spending, set saving
+                      goals, and receive notifications to stay on top of your budget.
+                    </span>
+                  </div>
                 </div>
               </div>
-              <div className='features__content-cards'>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>Investment Insights</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
-                  </div>
-                  <span>
-                    Stay informed about market trends and make smart investment decisions with our investment insights.
-                    Access real-time data, research potential opportunities, and optimize your investment portfolio.
-                  </span>
-                </div>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>Expense Tracking</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
-                  </div>
-                  <span>
-                    Simplify your financial life by tracking your expenses effortlessly. Categorize your spending, set saving
-                    goals, and receive notifications to stay on top of your budget.
-                  </span>
-                </div>
-              </div>
-            </div>
-            
+
             )}
             {selectedFeature === 'Customer Support' && (
               <div className='features__content'>
-              <div className='features__content-cards'>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>Dedicated Support Team</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
+                <div className='features__content-cards'>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>Dedicated Support Team</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Our dedicated customer support team is available 24/7 to assist you with any inquiries or issues you may
+                      encounter. Get personalized assistance and timely solutions to ensure a seamless banking experience.
+                    </span>
                   </div>
-                  <span>
-                    Our dedicated customer support team is available 24/7 to assist you with any inquiries or issues you may
-                    encounter. Get personalized assistance and timely solutions to ensure a seamless banking experience.
-                  </span>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>Live Chat Assistance</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Connect with our support team in real-time through our live chat feature. Receive instant help, ask questions,
+                      and get the support you need without leaving the comfort of your home.
+                    </span>
+                  </div>
                 </div>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>Live Chat Assistance</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
+                <div className='features__content-cards'>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>Knowledge Base</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Explore our extensive knowledge base to find answers to common questions and learn more about our banking
+                      products and services. Access helpful articles and tutorials at your convenience.
+                    </span>
                   </div>
-                  <span>
-                    Connect with our support team in real-time through our live chat feature. Receive instant help, ask questions,
-                    and get the support you need without leaving the comfort of your home.
-                  </span>
+                  <div className='features__content-card'>
+                    <div>
+                      <h1>Phone Support</h1>
+                      <img src={featuresArrow} alt='featuresArrow' />
+                    </div>
+                    <span>
+                      Need to speak with a support representative? Contact our phone support line for direct assistance. Our
+                      knowledgeable team is ready to help you over the phone.
+                    </span>
+                  </div>
                 </div>
               </div>
-              <div className='features__content-cards'>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>Knowledge Base</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
-                  </div>
-                  <span>
-                    Explore our extensive knowledge base to find answers to common questions and learn more about our banking
-                    products and services. Access helpful articles and tutorials at your convenience.
-                  </span>
-                </div>
-                <div className='features__content-card'>
-                  <div>
-                    <h1>Phone Support</h1>
-                    <img src={featuresArrow} alt='featuresArrow' />
-                  </div>
-                  <span>
-                    Need to speak with a support representative? Contact our phone support line for direct assistance. Our
-                    knowledgeable team is ready to help you over the phone.
-                  </span>
-                </div>
-              </div>
-            </div>
-            
+
             )}
           </div>
         </div>
--- yourbank/src/components/home/questions/Questions.jsx
+++ yourbank/src/components/home/questions/Questions.jsx
@@ -1,63 +1,83 @@
-import './questions.css'
-import Arrow from '../../../img/Assets/arrowdown.svg'
+import React, { useState } from 'react';
+import './questions.css';
+import Arrow from '../../../img/Assets/arrowdown.svg';
 
 function Questions() {
-    return(
-        <section className='questions' >
+    const [showAdditionalContent, setShowAdditionalContent] = useState(false);
+
+    const toggleAdditionalContent = () => {
+        setShowAdditionalContent(!showAdditionalContent);
+    };
+
+    return (
+        <section className='questions'>
             <div className='container'>
                 <div className='questions__container'>
                     <div className='questions__title'>
                         <h1><span>Frequently</span> Asked Questions</h1>
-                        <p>Still you have any questions? Contact our Team via support@yourbank.com</p>
+                        <p>Still have any questions? Contact our Team via support@yourbank.com</p>
                     </div>
                     <div className='questions__item-container'>
                         <div className='questions__row' >
                             <div className='questions__item' >
                                 <h1>How do I open an account with YourBank?</h1>
-                                <p>Opening an account with YourBank is easy. Simply
-                                     visit our website and click on the "Open an Account"
-                                      button. Follow the prompts, provide the required
-                                       information, and complete the application process.
-                                        If you have any questions or need assistance, our
-                                         customer support team is available to help.</p>
+                                <p>Opening an account with YourBank is easy. Simply visit our website and click on the "Open an Account" button. Follow the prompts, provide the required information, and complete the application process. If you have any questions or need assistance, our customer support team is available to help.</p>
                             </div>
                             <div className='questions__item' >
                                 <h1>What documents do I need to provide to apply for a loan?</h1>
-                                <p>The documents required for a loan application may vary depending
-                                     on the type of loan you are applying for. Generally, you will
-                                      need to provide identification documents (such as a passport
-                                       or driver's license), proof of income (such as pay stubs or tax returns),
-                                        and information about the collateral (if applicable). Our loan officers
-                                         will guide you through the specific requirements during the application
-                                         process.</p>
+                                <p>The documents required for a loan application may vary depending on the type of loan you are applying for. Generally, you will need to provide identification documents (such as a passport or driver's license), proof of income (such as pay stubs or tax returns), and information about the collateral (if applicable). Our loan officers will guide you through the specific requirements during the application process.</p>
                             </div>
                         </div>
                         <div className='questions__row' >
                             <div className='questions__item' >
                                 <h1>How can I access my accounts online?</h1>
-                                <p>Accessing your accounts online is simple and secure. Visit our website and click
-                                     on the "Login" button. Enter your username and password to access your accounts.
-                                      If you haven't registered for online banking, click on the "Enroll Now" button
-                                       and follow the registration process. If you need assistance, our customer support
-                                        team is available to guide you.</p>
+                                <p>Accessing your accounts online is simple and secure. Visit our website and click on the "Login" button. Enter your username and password to access your accounts. If you haven't registered for online banking, click on the "Enroll Now" button and follow the registration process. If you need assistance, our customer support team is available to guide you.</p>
                             </div>
                             <div className='questions__item' >
                                 <h1>Are my transactions and personal information secure?</h1>
-                                <p>At YourBank, we prioritize the security of your transactions
-                                     and personal information. We employ industry-leading encryption
-                                      and multi-factor authentication to ensure that your data is
-                                      protected. Additionally, we regularly update our security measures
-                                      to stay ahead of emerging threats. You can bank with confidence
-                                      knowing that we have robust security systems in place.</p>
+                                <p>At YourBank, we prioritize the security of your transactions and personal information. We employ industry-leading encryption and multi-factor authentication to ensure that your data is protected. Additionally, we regularly update our security measures to stay ahead of emerging threats. You can bank with confidence knowing that we have robust security systems in place.</p>
                             </div>
                         </div>
                         <div className='questions__row-background'></div>
                     </div>
-                    <div className='questions__button-container' ><button className='questions__button' >Load All FAQ’s <img src={Arrow} alt="Arrow" /></button></div>
+                    {showAdditionalContent && (
+                        <div className='questions__item-container' >
+                            <div className='questions__row'>
+                                <div className='questions__item'>
+                                    <h1>How do I apply for a credit card?</h1>
+                                    <p>Applying for a credit card with YourBank is a straightforward process. You can visit our credit card section on the website and choose the card that suits your needs. Click on the "Apply Now" button, fill out the required information, and submit your application. Our team will review your application, and if approved, you'll receive your new credit card shortly.</p>
+                                </div>
+                                <div className='questions__item'>
+                                    <h1>What benefits come with YourBank credit cards?</h1>
+                                    <p>YourBank credit cards come with various benefits, including cashback rewards, travel perks, and low-interest rates. Each credit card has its unique features, so you can choose one that aligns with your spending habits and lifestyle. Explore our credit card offerings to discover the benefits associated with each card.</p>
+                                </div>
+                            </div>
+                            <div className='questions__row'>
+                                <div className='questions__item'>
+                                    <h1>Is there a mobile app for YourBank?</h1>
+                                    <p>Yes, YourBank offers a user-friendly mobile app that allows you to manage your accounts, transfer funds, pay bills, and more, all from the convenience of your mobile device. Simply download the YourBank app from the App Store or Google Play, log in with your credentials, and enjoy the convenience of banking on the go.</p>
+                                </div>
+                                <div className='questions__item'>
+                                    <h1>What steps should I take if I lose my debit card?</h1>
+                                    <p>If you lose your YourBank debit card, it's essential to act quickly to protect your account. Contact our customer support team immediately to report the loss, and they will guide you through the process of blocking the card and issuing a replacement. Additionally, monitor your account for any unauthorized transactions and report them promptly.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    )}
+                    <div className='questions__button-container'>
+                        <button className='questions__button' onClick={toggleAdditionalContent}>
+                            Load All FAQ’s <img src={Arrow} alt="Arrow" style={{ transform: showAdditionalContent ? 'rotate(180deg)' : 'rotate(0deg)', transition: 'transform 0.3s ease' }} />
+                        </button>
+
+
+                    </div>
+
+
                 </div>
             </div>
         </section>
     );
 }
 
-export default Questions
+export default Questions;
