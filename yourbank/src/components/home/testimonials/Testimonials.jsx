c79fefc4a9e6abe8c707e76c73befd8fb2e6ef0f
Vendigo <119477860+Vendifo@users.noreply.github.com>
2023-12-10 07:25:18 +0900

    Сделан функционал в компоненте testimonials

--- yourbank/src/components/home/testimonials/Testimonials.jsx
+++ yourbank/src/components/home/testimonials/Testimonials.jsx
@@ -1,70 +1,102 @@
-import './testimonials.css'
-import arrowleft from '../../../img/Assets/arrowleft.svg'
-import arrowright from '../../../img/Assets/arrowright.svg'
-import testimonialscard from '../../../img/Assets/testimonialscard.svg'
+import React, { useState } from 'react';
+import './testimonials.css';
+import arrowleft from '../../../img/Assets/arrowleft.svg';
+import arrowright from '../../../img/Assets/arrowright.svg';
+import testimonialscard from '../../../img/Assets/testimonialscard.svg';
 
-function Testimonials () {
+function Testimonials() {
+    const [content, setContent] = useState('individuals');
+
+    const handleButtonClick = (contentType) => {
+        setContent(contentType);
+    };
+
     return (
         <section className='testimonials'>
-            <div className='container' >
+            <div className='container'>
                 <div className='testimonials__container'>
                     <div className='testimonials__info-container'>
-                        <div className='testimonials__text-container' >
+                        <div className='testimonials__text-container'>
                             <h1>Our <span>Testimonials</span></h1>
                             <p>Discover how YourBank has transformed lives with
-                                 innovative digital solutions and personalized
-                                  customer service. See why our clients trust
-                                   us for a secure and prosperous financial journey</p>
+                                innovative digital solutions and personalized
+                                customer service. See why our clients trust
+                                us for a secure and prosperous financial journey</p>
                         </div>
                         <div className='testimonials__tabs'>
-                            <button className='testimonials__button-individuals' >For Individuals</button>
-                            <button className='testimonials__button-businesses' >For Businesses</button>
+                            <button
+                                className={content === 'individuals' ? 'testimonials__button-individuals active' : 'testimonials__button-individuals'}
+                                onClick={() => handleButtonClick('individuals')}
+                            >
+                                For Individuals
+                            </button>
+                            <button
+                                className={content === 'businesses' ? 'testimonials__button-businesses active' : 'testimonials__button-businesses'}
+                                onClick={() => handleButtonClick('businesses')}
+                            >
+                                For Businesses
+                            </button>
                         </div>
                     </div>
                     <div className='testimonials__slider'>
-                        <button className='testimonials__button' ><img src={arrowleft} alt="arrowleft" /></button>
+                        <button className='testimonials__button'><img src={arrowleft} alt="arrowleft" /></button>
                         <div className='testimonials__item-container'>
-                            <div className='testimonials__card' >
-                                <div className='testimonials__card-images' >
-                                    <div className='item__line' ></div>
-                                    <img src={testimonialscard} alt="testimonialscard" />
-                                    <div className='item__line' ></div>
-                                </div>
-                                <p>YourBank has been my trusted financial partner for years. 
-                                    Their personalized service and innovative digital banking 
-                                    solutions have made managing my finances a breeze.</p>
-                                <span>Sara T</span>
-                            </div>
-                            <div className='testimonials__card' >
-                                <div className='testimonials__card-images' >
-                                    <div className='item__line' ></div>
-                                    <img src={testimonialscard} alt="testimonialscard" />
-                                    <div className='item__line' ></div>
-                                </div>
-                                <p>I recently started my own business, and YourBank has been 
-                                    instrumental in helping me set up my business accounts 
-                                    and secure the financing I needed. Their expert guidance 
-                                    and tailored solutions have been invaluable.</p>
-                                <span>John D</span>
-                            </div>
-                            <div className='testimonials__card' >
-                                <div className='testimonials__card-images' >
-                                    <div className='item__line' ></div>
-                                    <img src={testimonialscard} alt="testimonialscard" />
-                                    <div className='item__line' ></div>
-                                </div>
-                                <p>I love the convenience of YourBank's mobile banking app. 
-                                    It allows me to stay on top of my finances and make 
-                                    transactions on the go. The app is user-friendly and 
-                                    secure, giving me peace of mind.</p>
-                                <span>Emily G</span>
-                                
-                            </div>
-                            <div className='testimonials__fadeoutleft' ></div>
-                            <div className='testimonials__fadeouright' ></div>
-                            
+                            {content === 'individuals' && (
+                                <>
+                                    <div className='testimonials__card'>
+                                        
+                                        <p>YourBank has been my trusted financial partner for years.
+                                            Their personalized service and innovative digital banking
+                                            solutions have made managing my finances a breeze.</p>
+                                        <span>Sara T</span>
+                                    </div>
+                                    <div className='testimonials__card'>
+                                        
+                                        <p>YourBank has been my trusted financial partner for years.
+                                            Their personalized service and innovative digital banking
+                                            solutions have made managing my finances a breeze.</p>
+                                        <span>Sara T</span>
+                                    </div>
+                                    <div className='testimonials__card'>
+                                        
+                                        <p>YourBank has been my trusted financial partner for years.
+                                            Their personalized service and innovative digital banking
+                                            solutions have made managing my finances a breeze.</p>
+                                        <span>Sara T</span>
+                                    </div>
+                                </>
+                            )}
+                            {content === 'businesses' && (
+                                <>
+                                    <div className='testimonials__card'>
+                                       
+                                        <p>I recently started my own business, and YourBank has been
+                                            instrumental in helping me set up my business accounts
+                                            and secure the financing I needed. Their expert guidance
+                                            and tailored solutions have been invaluable.</p>
+                                        <span>John D</span>
+                                    </div>
+                                    <div className='testimonials__card'>
+                                       
+                                        <p>I recently started my own business, and YourBank has been
+                                            instrumental in helping me set up my business accounts
+                                            and secure the financing I needed. Their expert guidance
+                                            and tailored solutions have been invaluable.</p>
+                                        <span>John D</span>
+                                    </div>
+                                    <div className='testimonials__card'>
+                                       
+                                        <p>I recently started my own business, and YourBank has been
+                                            instrumental in helping me set up my business accounts
+                                            and secure the financing I needed. Their expert guidance
+                                            and tailored solutions have been invaluable.</p>
+                                        <span>John D</span>
+                                    </div>
+                                    
+                                </>
+                            )}
                         </div>
-                        <button className='testimonials__button' ><img src={arrowright} alt="arrowright" /></button>
+                        <button className='testimonials__button'><img src={arrowright} alt="arrowright" /></button>
                     </div>
                 </div>
             </div>
@@ -72,4 +104,4 @@ function Testimonials () {
     );
 }
 
-export default Testimonials
+export default Testimonials;
